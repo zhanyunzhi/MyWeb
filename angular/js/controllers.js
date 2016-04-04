6fecba022564a2af47c84296e3b2e4b9a5a66f36
zhanyunzhi <540562332@qq.com>
2016-04-05 00:41:42 +0800

    commit20160405

--- angular/js/controllers.js
+++ angular/js/controllers.js
@@ -12,11 +12,41 @@ angular.module('ngApp',[]).controller('PhoneListCtrl',
     function($scope){
         $scope.phones = [
             {"name" : "Nexus S",
-                "snippet" : "Fast just got faster with Nexus S."},
+                "snippet" : "Fast just got faster with Nexus S.",
+                "age" : 0},
             {"name" : "Motorola XOOM with Wi-Fi",
-                "snippet" : "The Next,Next Generation tablet."},
+                "snippet" : "The Next,Next Generation tablet.",
+                "age" : 1},
             {"name" : "MOTOROLA XOOM",
-                "snippet" : "The Next,Next Generation tablet."}
+                "snippet" : "The Next,Next Generation tablet.",
+                "age" : 2}
         ];
-}
+
+        $scope.orderProp = "age";
+    }
+);
+angular.module("ngApp2",[]).controller("PhoneListCtrl2",
+    function($scope, $http){
+        //url是相对于我们的html文件的
+        $http.get("data/phone.json").success(function(data){
+            $scope.phones = data.splice(0, 5);
+        });
+        $scope.orderProp2 = "age";
+        $scope.imageUrl = "http://www.yanziw.com/templets/default/images/left_phone.gif";
+    }
+);
+angular.module('phonecat',[]).controller('PhoneListCtrl',
+    function($scope, $http){
+        //url是相对于我们的html文件的
+        $http.get("data/phone.json").success(function(data){
+            $scope.phones = data.splice(0, 5);
+        });
+        $scope.orderProp2 = "age";
+        $scope.imageUrl = "http://www.yanziw.com/templets/default/images/left_phone.gif";
+    }
+);
+angular.module("phonecat",[]).controller("PhoneDetailCtrl",
+    function($scope, $routeParams){
+        $scope.phoneId = $routeParams.phoneId;
+    }
 );
